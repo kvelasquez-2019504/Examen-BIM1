730579c7c20d33bf3ea883b9550f77a6201352af
Kenneth Velasquez <kvelasquez-2019504@kinal.edu.gt>
2024-02-26 08:56:49 -0600

    Se comenzo a reestructurar a esmodule

---
+++ configs/mongo.js
@@ -0,0 +1,32 @@
+'use strict'
+import mongoose from 'mongoose';
+
+export const dbConnection = async () => {
+    try {
+        mongoose.connection.on('error', () => {
+            console.log('MongoDB | Could not be connect to MongoDB');
+            mongoose.disconnect();
+        });
+        mongoose.connection.on('connecting', () => {
+            console.log('MongoDB | Try connecting');
+        });
+        mongoose.connection.on('connected', () => {
+            console.log('MongoDB | Connected to MongoDB');
+        });
+        mongoose.connection.once('open', () => {
+            console.log('MongoDB | Connected to database');
+        });
+        mongoose.connection.on('reconnected', () => {
+            console.log("MongoDB | Reconnected to MongoDB");
+        });
+        mongoose.connection.on('disconnected', () => {
+            console.log('MongoDB | disconnected');
+        });
+        await mongoose.connect(process.env.URI_MONGO, {
+            serverSelectionTimeOutMS: 5000,
+            maxPoolSize:50
+        })
+    } catch (error) {
+        console.log(error, " Database connection failed");
+    }
+}
--- models/sever.js
+++ models/sever.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const {dbConnection} = require("../db/config");
+const {dbConnection} = require("../db/config.js");
 
 class Server{
     constructor(){
