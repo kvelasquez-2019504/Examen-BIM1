5a3e0b71147eaa110146f91d7fc641cecef1053b
Kenneth Velasquez <kvelasquez-2019504@kinal.edu.gt>
2024-02-26 09:25:15 -0600

    Se levanto el servidor con esmodule

--- configs/server.js
+++ configs/server.js
@@ -4,5 +4,35 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
-import { dbConnection } from './mongo';
+import { dbConnection } from './mongo.js';
 
+class Server {
+    constructor(){
+        this.app = express();
+        this.port= process.env.PORT;
+        this.middlewares();
+    }
+
+    async conectarDB(){
+        await dbConnection();
+    }
+
+    middlewares(){
+        this.app.use(cors());
+        this.app.use(express.json());
+        this.app.use(helmet());
+        this.app.use(morgan('dev'));
+    }
+
+    routes(){
+        
+    }
+
+    listen(){
+        this.app.listen(this.port, ()=>{
+            console.log('Sever running on port '+this.port);
+        })
+    }
+}
+
+export default Server;
