3ef5c015547cc643e246f6f4605f1b5205e47270
Kenneth Velasquez <kvelasquez-2019504@kinal.edu.gt>
2024-03-04 11:53:38 -0600

    Se agrego verificacion de username unico

--- src/users/user.routes.js
+++ src/users/user.routes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import {check} from 'express-validator';
 import { validateFiels } from '../middlewares/validate-fields.js';
+import { existsUsername } from '../helpers/db-validator.js';
 import { userPost } from './user.controller.js';
 const router = Router();
 
 router.post('/',[
     check("username","The username is required for the account").not().isEmpty(),
+    check("username").custom(existsUsername),
     validateFiels,
 ],userPost);
 
