e6cddd4e7da037379edf5c74b0242b45ddb81a4a
Kenneth Velasquez <kvelasquez-2019504@kinal.edu.gt>
2024-03-04 10:40:14 -0600

    Se agrego userModel

---
+++ src/users/user.model.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+export const UserSchema = mongoose.Schema({
+    username:{
+        type:String,
+        required:[true,'The username is required for account']
+    },
+    name:{
+        type:String,
+        required:[true, 'Your name complet is required']
+    },
+    password:{
+        type:String,
+        required:[true,'A password is required for security']
+    },
+    age:{
+        type:Number,
+        required:[true,'Your age is required for your account']
+    },
+    role:{ 
+        type:String,
+        default:"CLIENT"
+    },
+    state:{
+        type:Boolean,
+        default:true
+    }
+});
+
+UserSchema.method.JSON= function(){
+    const {__v,_id,...user}=this.toObject();
+    user.uid=_id;
+    return user;
+}
+
+export default mongoose.model('User',UserSchema);
