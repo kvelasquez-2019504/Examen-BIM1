8f4b68c51efbfd7b94b7a4555358d87636c8ec49
Kenneth Velasquez <kvelasquez-2019504@kinal.edu.gt>
2024-03-07 20:35:29 -0600

    Se arreglo error de escritura

--- src/users/user.routes.js
+++ src/users/user.routes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import {check} from 'express-validator';
-import { validateFiels } from '../middlewares/validate-fields.js';
+import { validateFields } from '../middlewares/validate-fields.js';
 import { existsUsername,validateAgeUser } from '../helpers/db-validator.js';
 import { userPost } from './user.controller.js';
 const router = Router();
@@ -12,7 +12,7 @@ router.post('/',[
     check('age','your age is mandatory for the account').not().isEmpty(),
     check('age').custom(validateAgeUser),
     check('password','The password is mandatory and with a minimum of 6 characters').isLength({min:6}),
-    validateFiels,
+    validateFields,
 ],userPost);
 
 export default router;
