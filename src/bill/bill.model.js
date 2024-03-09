6ed538196d65a353bda6a071d08a7b8f38fe2075
Kenneth Velasquez <kvelasquez-2019504@kinal.edu.gt>
2024-03-08 19:52:20 -0600

    Se creo modelo bill

---
+++ src/bill/bill.model.js
@@ -0,0 +1,36 @@
+import mongoose, {Schema} from 'mongoose';
+
+const BillSchema = Schema({
+    idUser:{
+        type:Schema.Types.ObjectId,
+        required:[true,"The user id required"]
+    },
+    products:{
+        type:Array,
+        default:[]
+    },
+    fullPayment:{
+        type:Number,
+        default:0
+    },
+    pay:{
+        type:Number,
+        default:0
+    },
+    totalItems:{
+        type:Number,
+        default:0
+    },
+    state:{
+        type:Boolean,
+        default:true
+    }
+});
+
+BillSchema.method.JSON=function(){
+    const {__v,_id,state,...bill}=this.toObject();
+    bill.uid=_id;
+    return bill;
+}
+
+export default mongoose.model('Bill',BillSchema);
